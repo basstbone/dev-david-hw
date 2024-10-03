ae17efb298815deb375e101c6fbc4255a14c6ed5
basstbone <149620289+basstbone@users.noreply.github.com>
2024-10-03 08:52:55 -0500

    update

---
+++ scripts/scripts.js
@@ -0,0 +1,14 @@
+body {
+    font-family: 'Roboto', sans-serif;
+    background-image: url('../img/background-pattern.png');
+    background-size: cover;
+  }
+  
+  #game-board {
+    border: 2px solid #000;
+  }
+  
+  #game-piece {
+    border-radius: 5px;
+  }
+  
